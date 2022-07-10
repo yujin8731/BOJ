848be283803a2be3bbba87d40e4906212d7a397b
yujin yun <yujin8731@daum.net>
2022-07-10 14:51:13 +0900

    Add 1001.py

---
+++ 2338.py
@@ -0,0 +1,7 @@
+# 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A×B를 출력한다
+A=int(input())
+B=int(input())
+
+print(A+B)
+print(A-B)
+print(A*B)
---
+++ 2739.py
@@ -0,0 +1,3 @@
+n=int(input("숫자 입력"))
+for i in range(1,10):
+    print(n, "*", i, "=", i*n)
